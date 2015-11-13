59298040481cba49c40b237fffe59c1c53ab4e31
plusmancn <plusmancn@gmail.com>
2015-11-13 10:42:23 +0800

    md5性能测试

---
+++ benchmark/Md5Alias.test.js
@@ -0,0 +1,15 @@
+'use strict';
+/**
+ * @desc md5 hash 性能测试
+ */
+
+var crypto = require('crypto');
+var DEBUG_MD5 = require('debug')('md5');
+
+console.time('15W MD5 TEST');
+for(let i=0;i<1.5e5;i++){
+  var md5 = crypto.createHash('md5');
+  md5.update('18667903755');
+  DEBUG_MD5('Time %d was %s',i,md5.digest('hex'));
+}
+console.timeEnd('15W MD5 TEST');
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 /**
- * @author: JPush
- * @Date: 14-6-24
- * @version: 3.1.0
+ * @author: JPush <plusmancn@gmail.com>
+ * @Date: 2015-11-13
+ * @version: 4.0.0
  */
-module.exports = require('./lib/JPush/JPush.js');
+
+// 基础推送服务
+module.exports.push = require('./lib/JPush/JPush.js');
+// 推送队列服务
+module.exports.queue = function(){};
+// 模块推送性能监控盘
+module.exports.dashboard = function(){};
