392fe3ea93e2962ef2335c31ae1275fd478472df
plusmancn <plusmancn@gmail.com>
2015-11-23 14:25:37 +0800

    oj test

--- index.js
+++ index.js
@@ -7,6 +7,6 @@
 // 基础推送服务
 module.exports.push = require('./lib/JPush/JPush.js');
 // 推送队列服务
-module.exports.queue = function(){};
+module.exports.queue = require('./lib/task_pool/queue.js');
 // 模块推送性能监控盘
 module.exports.dashboard = function(){};
